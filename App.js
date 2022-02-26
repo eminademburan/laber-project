1bd8fd6b61df3ecc51cba2ad549827078d979278
DESKTOP-J6PID05\adems <ademsan99@gmail.com>
2022-02-26 13:28:51 +0300

    Revert "initialize app gui"
    This reverts commit d9e966c09284841875556a7099ccd5ca7ad6b5ca.

--- App.js
+++ App.js
@@ -1,79 +1,112 @@
-import * as React from 'react';
-import { Text, View, Button , StyleSheet, TextInput, TouchableOpacity, SafeAreaView} from 'react-native';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import {NavigationContainer } from '@react-navigation/native'
-import {createNativeStackNavigator} from '@react-navigation/native-stack'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
 
-// You can import from local files
-import Profile from './components/Profile.js';
-import Home from './components/Home.js';
-import SignUp from './components/SignUp.js';
-import Settings from './components/Settings.js';
-import Tasks from './components/Tasks.js';
+import React from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
 
-import axios from 'axios';
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
 
-
-
-
-const Stack = createNativeStackNavigator();
-
-const Tab = createBottomTabNavigator();
-
-function MyTabs( navigation) {
+const Section = ({children, title}): Node => {
+  const isDarkMode = useColorScheme() === 'dark';
   return (
-      <Tab.Navigator
-          initialRouteName="Feed"
-          screenOptions={{
-            tabBarActiveTintColor: '#e91e63',
-          }}>
-        <Tab.Screen
-            name="Profile"
-            component={Profile}
-            options={{
-              tabBarLabel: 'Home',
-              tabBarIcon: ({ color, size }) => (
-                  <MaterialCommunityIcons name="account-settings" color={color} size={size} />
-              ),
-            }}/>
-        <Tab.Screen
-            name="Tasks"
-            component={Tasks}
-            options={{
-              tabBarLabel: 'Tasks',
-              tabBarIcon: ({ color, size }) => (
-                  <MaterialCommunityIcons name="store" color={color} size={size} />
-              ),
-            }}/>
-        <Tab.Screen
-            name="Settings"
-            component={Tasks}
-            options={{
-              tabBarLabel: 'Home',
-              tabBarIcon: ({ color, size }) => (
-                  <MaterialCommunityIcons name="account-settings-outline" color={color} size={size} />
-              ),
-            }}/>
-      </Tab.Navigator>
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
   );
-}
+};
 
-export default class App extends React.Component{
+const App: () => Node = () => {
+  const isDarkMode = useColorScheme() === 'dark';
 
-  render()
-  {
-    return (
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
 
-        <NavigationContainer>
-          <Stack.Navigator screenOptions={{headerShown: false }}>
-            <Stack.Screen name="Home" component={Home} ></Stack.Screen>
-            <Stack.Screen name="SignUp" component={SignUp}></Stack.Screen>
-            <Stack.Screen name="MyTabs" component={MyTabs}></Stack.Screen>
-          </Stack.Navigator>
-        </NavigationContainer>
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Section title="Step One">
+            Edit <Text style={styles.highlight}>App.js</Text> to change this
+            screen and then come back to see your edits.
+          </Section>
+          <Section title="See Your Changes">
+            <ReloadInstructions />
+          </Section>
+          <Section title="Debug">
+            <DebugInstructions />
+          </Section>
+          <Section title="Learn More">
+            Read the docs to discover what to do next:
+          </Section>
+          <LearnMoreLinks />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
-    )
-  }
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
 
-}
+export default App;
