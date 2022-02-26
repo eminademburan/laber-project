5d367ce4ac9cafebb79228fe2c9a25f9ebc3a25b
DESKTOP-J6PID05\adems <ademsan99@gmail.com>
2022-02-26 13:28:18 +0300

    some changes

--- components/Tasks.js
+++ components/Tasks.js
@@ -10,6 +10,7 @@ import {
     CheckBox,
     Component,
 } from 'react-native';
+
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
